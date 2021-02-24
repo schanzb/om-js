827b04a065845ff2c7c95a1773dd82aea45c4797
Ben Schanzer <benschanzer@icloud.com>
2021-02-24 13:54:07 -0500

    2.14 done

--- w2/GiphySearch/javascript/main.js
+++ w2/GiphySearch/javascript/main.js
@@ -1,9 +1,21 @@
 /*  1. Grab input */
 
-
+// When go button is clicked
 document.querySelector(".js-go").addEventListener('click',function(){
     var input = document.querySelector("input").value;
     pushToDOM(input);
+
+});
+
+document.querySelector(".js-userinput").addEventListener('keyup',function(e){
+
+    var input = document.querySelector("input").value;
+
+    //if ENTER is pressed
+    if(e.which === 13){
+        pushToDOM(input);
+    }
+
 
 });
 

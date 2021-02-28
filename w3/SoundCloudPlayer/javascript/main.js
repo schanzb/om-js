e6a439c21720879284dec4d50b5c7b9d5f746540
Ben Schanzer <benschanzer@icloud.com>
2021-02-28 16:31:33 -0500

    added static button alone, to be replaced with chris' code for accuracy sake

--- w3/SoundCloudPlayer/javascript/main.js
+++ w3/SoundCloudPlayer/javascript/main.js
@@ -32,10 +32,55 @@ SoundCloudAPI.getTrack("Rilo Kiley");
 SoundCloudAPI.renderTracks = function() {
     // makes .card div
     var card = document.createElement('div');
-    card.classList.add('card');
+    card.classList.add('card', 'card-js');
 
     var searchResults = document.querySelector('.js-search-results');
     searchResults.appendChild(card);
+
+
+    // make .image div
+    var image = document.createElement('div');
+    image.classList.add('image-js');
+
+    var cardDiv = document.querySelector('.card-js');
+    cardDiv.appendChild(image);
+    // put in image
+    var imageDiv = document.querySelector('.image-js')
+    imageDiv.innerHTML = "<img class='image_img' src='http://www.placekitten.com/290/290'>";
+
+
+    // make .content div
+    var content = document.createElement('div');
+    content.classList.add('content', 'content-js');
+
+    cardDiv.appendChild(content);
+
+    
+    // make .header div
+    var header = document.createElement('div');
+    header.classList.add('header', 'header-js');
+
+    var contentDiv = document.querySelector('.content-js');
+    contentDiv.appendChild(header);
+    // put in title
+    var headerDiv = document.querySelector('.header-js');
+    headerDiv.innerHTML = '<a href="https://soundcloud.com/barsuk-records/rilo-kiley-science-vs-romance" target="_blank">"Science Vs. Romance"</a>';
+
+
+    // make add to playlist button
+    var button = document.createElement('div');
+    button.classList.add('ui', 'bottom' , 'attached' , 'button', 'js-button', 'button-js');
+
+    cardDiv.appendChild(button);
+    // add icon
+    var buttonDiv = document.querySelector('.button-js');
+    // var icon = document.createElement('i');
+    // icon.classList.add('add icon');
+    
+    buttonDiv.innerHTML = '<i class="add icon"></i>';
+    buttonDiv.innerHTML = '<span>Add to playlist</span>';
+
+
 };
 SoundCloudAPI.renderTracks();
 

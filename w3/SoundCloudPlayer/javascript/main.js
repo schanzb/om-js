bc619d42a5032d74e6eb97112aad3d0550902d01
Ben Schanzer <benschanzer@icloud.com>
2021-02-28 15:54:38 -0500

    halfway through 3.9

--- w3/SoundCloudPlayer/javascript/main.js
+++ w3/SoundCloudPlayer/javascript/main.js
@@ -7,16 +7,37 @@
 // Query Sound Cloud API
 
 
-SC.initialize({
-  client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
-});
+var SoundCloudAPI = {};
 
-// find all sounds of buskers licensed under 'creative commons share alike'
-SC.get('/tracks', {
-  q: 'buskers'
-}).then(function(tracks) {
-  console.log(tracks);
-});
+SoundCloudAPI.init = function(){
+    SC.initialize({
+        client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
+    });
+};
+
+SoundCloudAPI.init();
+
+SoundCloudAPI.getTrack = function(inputValue){
+    // find all sounds of buskers licensed under 'creative commons share alike'
+    SC.get('/tracks', {
+        q: inputValue
+    }).then(function(tracks) {
+        console.log(tracks);
+    });
+};
+
+SoundCloudAPI.getTrack("Rilo Kiley");
+
+
+SoundCloudAPI.renderTracks = function() {
+    // makes .card div
+    var card = document.createElement('div');
+    card.classList.add('card');
+
+    var searchResults = document.querySelector('.js-search-results');
+    searchResults.appendChild(card);
+};
+SoundCloudAPI.renderTracks();
 
 
 

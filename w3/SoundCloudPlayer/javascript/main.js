323f5c3dd03273e2d7bb1605749786fb9af2147e
Ben Schanzer <benschanzer@icloud.com>
2021-02-28 14:53:07 -0500

    added week 3, started week 3 projects

---
+++ w3/SoundCloudPlayer/javascript/main.js
@@ -0,0 +1,34 @@
+// Search
+
+
+
+
+
+// Query Sound Cloud API
+
+
+SC.initialize({
+  client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
+});
+
+// find all sounds of buskers licensed under 'creative commons share alike'
+SC.get('/tracks', {
+  q: 'buskers'
+}).then(function(tracks) {
+  console.log(tracks);
+});
+
+
+
+
+
+// Display Cards
+
+
+
+
+
+
+
+
+// Add to playlist and play

5bbcedf2d2884f2fe5321f62c69a53dff0688996
Ben Schanzer <benschanzer@icloud.com>
2021-02-28 21:10:44 -0500

    starting homework

--- w3/SoundCloudPlayer/javascript/main.js
+++ w3/SoundCloudPlayer/javascript/main.js
@@ -1,5 +1,14 @@
 // Search
+var UI = {};
 
+UI.EnterPress = function(){
+
+};
+
+
+UI.SubmitClick = function(){
+
+};
 
 
 
@@ -23,74 +32,89 @@ SoundCloudAPI.getTrack = function(inputValue){
         q: inputValue
     }).then(function(tracks) {
         console.log(tracks);
+        
+        SoundCloudAPI.renderTracks(tracks);
     });
 };
 
 SoundCloudAPI.getTrack("Rilo Kiley");
 
-
-SoundCloudAPI.renderTracks = function() {
-    // card
-    var card = document.createElement('div');
-    card.classList.add('card');
-
-    // make .image div
-    var imageDiv = document.createElement('div');
-    imageDiv.classList.add('image');
-
-    var image_img = document.createElement('img');
-    image_img.classList.add('image_img');
-    image_img.src = 'http://www.placekitten.com/290/290';
-
-    imageDiv.appendChild(image_img);
-
-
-    // content
-    var content = document.createElement('div');
-    content.classList.add('content');
-
-    var header = document.createElement('div');
-    header.classList.add('header');
-    header.innerHTML = '<a href="#" target="_blank">"Science Vs. Romance"</a>'
-
-    // button
-    var button = document.createElement('div');
-    button.classList.add('ui', 'bottom', 'attached', 'button', 'js-button');
-    
-    var icon = document.createElement('i');
-    icon.classList.add('add', 'icon');
-
-    var buttonText = document.createElement('span');
-    buttonText.innerHTML = 'Add to Playlist';
-
-    //apendChild
-    content.appendChild(header);
-
-    button.appendChild(icon);
-    button.appendChild(buttonText);
-
-    card.appendChild(imageDiv);
-    card.appendChild(content);
-    card.appendChild(button);
-
-    var searchResults = document.querySelector('.js-search-results');
-    searchResults.appendChild(card);
-    
-
-};
-SoundCloudAPI.renderTracks();
-
-
-
-
-
 // Display Cards
 
+SoundCloudAPI.renderTracks = function(tracks) {
+
+    tracks.forEach(function(track){
+        // card
+        var card = document.createElement('div');
+        card.classList.add('card');
+
+        // make .image div
+        var imageDiv = document.createElement('div');
+        imageDiv.classList.add('image');
+
+        var image_img = document.createElement('img');
+        image_img.classList.add('image_img');
+        image_img.src = track.artwork_url || 'http://lorempixel.com/200/200/abstract/';
+
+        imageDiv.appendChild(image_img);
 
 
+        // content
+        var content = document.createElement('div');
+        content.classList.add('content');
+
+        var header = document.createElement('div');
+        header.classList.add('header');
+        header.innerHTML = '<a href="' + track.permalink_url + '" target="_blank">"' + track.title + '"</a>';
+
+        // button
+        var button = document.createElement('div');
+        button.classList.add('ui', 'bottom', 'attached', 'button', 'js-button');
+
+        var icon = document.createElement('i');
+        icon.classList.add('add', 'icon');
+
+        var buttonText = document.createElement('span');
+        buttonText.innerHTML = 'Add to Playlist';
+
+        //apendChild
+        content.appendChild(header);
+
+        button.appendChild(icon);
+        button.appendChild(buttonText);
+
+        button.addEventListener('click', function(){
+            SoundCloudAPI.getEmbed(track.permalink_url);
+        });
+
+        card.appendChild(imageDiv);
+        card.appendChild(content);
+        card.appendChild(button);
+
+        var searchResults = document.querySelector('.js-search-results');
+        searchResults.appendChild(card);
+    });
+
+};
 
 
+SoundCloudAPI.getEmbed = function(trackURL){
+    SC.oEmbed(trackURL, {
+        auto_play: false
+      }).then(function(embed){
+        // console.log('oEmbed response: ', embed);
+      
+        var sideBar = document.querySelector('.js-playlist');
+        
+        
+
+        var box = document.createElement('div');
+        box.innerHTML = embed.html; 
+        sideBar.insertBefore(box, sideBar.firstChild);
+        localStorage.setItem('key', sideBar.innerHTML);
 
 
-
-// Add to playlist and play
+      });
+};
+var sideBar = document.querySelector('.js-playlist');
+sideBar.innerHTML = localStorage.getItem("key");
